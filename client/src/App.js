1d5d59cb6c7b44dc352e06be72ba5c1f68ecabbe
daniel <danielkrg11@gmail.com>
2025-02-15 15:33:44 -0800

    Added term parameter for API call

--- client/src/App.js
+++ client/src/App.js
@@ -16,43 +16,6 @@ function Home() {
   );
 }
 
-// function UserData() {
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-//     axios
-//       .get("http://localhost:5001/userdata", { withCredentials: true }) // Fetch from backend
-//       .then((response) => setData(response.data))
-//       .catch((error) => console.error("Error fetching data:", error));
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Your Top Tracks & Artists</h1>
-//       {data ? (
-//         <>
-//           <h2>Top Tracks</h2>
-//           <ol>
-//             {data.topTracks.map((track, index) => (
-//               <li key={index}>
-//                 {track.name} by {track.artists[0].name}
-//               </li>
-//             ))}
-//           </ol>
-
-//           <h2>Top Artists</h2>
-//           <ol>
-//             {data.topArtists.map((artist, index) => (
-//               <li key={index}>{artist.name}</li>
-//             ))}
-//           </ol>
-//         </>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-// }
 
 function App() {
   return (
