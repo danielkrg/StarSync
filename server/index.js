3408d9dd147bb7ada35b175eb0cb691609b403f9
daniel <danielkrg11@gmail.com>
2025-02-17 22:15:32 -0800

    cleaned up default react files

--- server/index.js
+++ server/index.js
@@ -59,15 +59,18 @@ app.get('/callback', async (req, res) => {
 
 app.get('/userdata', async (req, res) => {
     const accessToken = req.session.accessToken;
+    const timeRange = req.query.time_range || "long_term";
     if (!accessToken) return res.status(401).send('Not authenticated');
 
     try {
         const [tracksResponse, artistsResponse] = await Promise.all([
-            axios.get('https://api.spotify.com/v1/me/top/tracks?limit=5', {
-                headers: { Authorization: `Bearer ${accessToken}` }
+            axios.get('https://api.spotify.com/v1/me/top/tracks', {
+                headers: { Authorization: `Bearer ${accessToken}` },
+                params: { limit: 5, time_range: timeRange },
             }),
-            axios.get('https://api.spotify.com/v1/me/top/artists?limit=5', {
-                headers: { Authorization: `Bearer ${accessToken}` }
+            axios.get('https://api.spotify.com/v1/me/top/artists?', {
+                headers: { Authorization: `Bearer ${accessToken}` },
+                params: { limit: 5, time_range: timeRange },
             })
         ]);
 
