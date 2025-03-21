2035438ce618b2e2decc38dff5bd10fcea26d3d5
daniel <danielkrg11@gmail.com>
2025-03-21 00:19:10 -0700

    testing commit

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -3,24 +3,56 @@ import { useUserData } from "./UserDataContext";
 function Dashboard() {
     const { longTermData, shortTermData } = useUserData();
 
-    if (!longTermData || !shortTermData) return <h1>Loading Data...</h1>;
+    if (!longTermData || !shortTermData) {
+        return "There seems to be an issue finding any data..."
+    }
 
     return (
         <div>
-            <h1>Hello, {longTermData.displayName}</h1>
-            <h1>Your Top 10 Tracks</h1>
-            <ul>
-                {longTermData.topTracks?.map((track, index) => (
-                    <li key={index}>{track.name} by {track.artists[0]}</li>
-                ))}
-            </ul>
+            <h1>Hello {shortTermData.displayName}</h1>
+            <h1>Your Top 10 Tracks Of All Time:</h1>
+            {longTermData.topTracks && longTermData.topTracks.length > 0 ? (
+                <ul>
+                    {longTermData.topTracks?.map((track, index) => (
+                        <li key={index}>{track.name} by {track.artists[0]}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>You don't have any long term top track data yet</p>
+            )}
 
-            <h1>Your Top 10 Artists</h1>
-            <ul>
-                {longTermData.topArtists?.map((artist, index) => (
-                    <li key={index}>{artist.name}</li>
-                ))}
-            </ul>
+            <h1>Your Top 10 Artists Of All Time:</h1>
+            {longTermData.topArtists && longTermData.topArtists.length > 0 ? (
+                <ul>
+                    {longTermData.topArtists?.map((artist) => (
+                        <li key={artist}>{artist.name} </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>You don't have any long term top artist data yet</p>
+            )}
+
+            <h1>Your Top 10 Tracks Lately:</h1>
+            {shortTermData.topTracks && shortTermData.topTracks.length > 0 ? (
+                <ul>
+                    {shortTermData.topTracks?.map((track, index) => (
+                        <li key={index}>{track.name} by {track.artists[0]}</li>
+                    ))}
+                </ul>
+            ) : (
+                <p>You don't have any short term top track data yet</p>
+            )}
+
+            <h1>Your Top 10 Artists Lately:</h1>
+            {shortTermData.topArtists && shortTermData.topArtists.length > 0 ? (
+                <ul>
+                    {shortTermData.topArtists?.map((artist) => (
+                        <li key={artist}>{artist.name} </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>You don't have any short term top artist data yet</p>
+            )}
         </div>
     );
 }
