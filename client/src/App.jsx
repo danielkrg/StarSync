417323f7f7312b14e99d543d3b93f42166e3baba
daniel <danielkrg11@gmail.com>
2025-03-29 01:36:51 -0700

    updated login styling, attempting custom animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,24 @@ function Home() {
   };
 
   return (
-    <div>
-      <h1>Spotify Stats</h1>
-      <button onClick={handleLogin}>Login with Spotify</button>
+  <div className="flex justify-center items-center h-screen">
+    <div className="flex flex-col items-center space-y-30">
+      <h1 
+        className="animate-fadeInOut text-5xl text-white font-bold"
+      >
+        Spotify Stats
+      </h1>
+      <button 
+        onClick={handleLogin}
+        className="border-2 border-pink-600 hover:border-green-500 
+        font-semibold text-pink-600 hover:text-green-500
+        py-2 px-4 rounded-full 
+        transition-all duration-300 ease-in-out"
+      >
+        Login with Spotify
+      </button>
     </div>
+  </div>
   );
 }
 
